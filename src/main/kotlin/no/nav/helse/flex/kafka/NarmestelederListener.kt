a62ef14122b20f56f72be5fbc1d790578922eaf2
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-10-07 12:08:58 +0200

    Setter ny group id

--- src/main/kotlin/no/nav/helse/flex/kafka/NarmestelederListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/NarmestelederListener.kt
@@ -1,6 +1,5 @@
 package no.nav.helse.flex.kafka
 
-import no.nav.helse.flex.logger
 import no.nav.helse.flex.narmesteleder.OppdateringAvNarmesteLeder
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
@@ -12,23 +11,13 @@ class NarmestelederListener(
     private val oppdateringAvNarmesteLeder: OppdateringAvNarmesteLeder
 ) {
 
-    private val log = logger()
-
     @KafkaListener(
         topics = [NARMESTELEDER_LEESAH_TOPIC],
         containerFactory = "aivenKafkaListenerContainerFactory",
     )
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
-        try {
-            oppdateringAvNarmesteLeder.behandleMeldingFraKafka(cr.value())
-            acknowledgment.acknowledge()
-        } catch (e: Exception) {
-            log.error(
-                "Feil ved mottak av record med key: ${cr.key()} offset: ${cr.offset()} partition: ${cr.partition()}",
-                e
-            )
-            throw e
-        }
+        oppdateringAvNarmesteLeder.behandleMeldingFraKafka(cr.value())
+        acknowledgment.acknowledge()
     }
 }
 
--- src/main/kotlin/no/nav/helse/flex/kafka/SykepengesoknadListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/SykepengesoknadListener.kt
@@ -6,33 +6,46 @@ import no.nav.helse.flex.objectMapper
 import no.nav.helse.flex.varsler.VarselPlanlegger
 import no.nav.syfo.kafka.felles.SykepengesoknadDTO
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.springframework.context.annotation.Profile
+import org.apache.kafka.common.TopicPartition
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.listener.ConsumerSeekAware
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneOffset
 
 @Component
-@Profile("test")
 class SykepengesoknadListener(
     val varselPlanlegger: VarselPlanlegger
-) {
+) : ConsumerSeekAware {
 
     private val log = logger()
 
     @KafkaListener(
         topics = [FLEX_SYKEPENGESOKNAD_TOPIC],
-        containerFactory = "aivenKafkaListenerContainerFactory"
+        containerFactory = "aivenKafkaListenerContainerFactory",
+        properties = ["auto.offset.reset = latest"],
+        groupId = "sykepengesoknad-ikke-sendt-altinnvarsel-2"
     )
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
-        try {
-            val soknad = cr.value().tilSykepengesoknadDTO()
-            varselPlanlegger.planleggVarsler(soknad, Instant.ofEpochMilli(cr.timestamp()))
-            acknowledgment.acknowledge()
-        } catch (e: Exception) {
-            log.error("Feil ved mottak av record med key: ${cr.key()} offset: ${cr.offset()} partition: ${cr.partition()}", e)
-            throw e
-        }
+        val soknad = cr.value().tilSykepengesoknadDTO()
+        varselPlanlegger.planleggVarsler(soknad, Instant.ofEpochMilli(cr.timestamp()))
+        acknowledgment.acknowledge()
+    }
+
+    override fun registerSeekCallback(callback: ConsumerSeekAware.ConsumerSeekCallback) {
+        // register custom callback
+        log.info("registerSeekCallback ${this.javaClass.simpleName}")
+    }
+
+    override fun onPartitionsAssigned(assignments: Map<TopicPartition, Long>, callback: ConsumerSeekAware.ConsumerSeekCallback) {
+        // Seek all the assigned partition to a certain offset
+        log.info("onPartitionsAssigned ${this.javaClass.simpleName}")
+
+        val sjetterOktober = LocalDate.of(2021, 10, 6).atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli()
+        callback.seekToTimestamp(assignments.keys, sjetterOktober)
+        log.info("ferdig med seek seekToTimestamp ${this.javaClass.simpleName}")
     }
 
     fun String.tilSykepengesoknadDTO(): SykepengesoknadDTO = objectMapper.readValue(this)
