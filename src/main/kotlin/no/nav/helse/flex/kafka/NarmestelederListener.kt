4e7b49790b2c8543184d1a934871eb7be9b88b9e
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2021-10-08 09:17:41 +0200

    Stopper konsumering

--- src/main/kotlin/no/nav/helse/flex/kafka/NarmestelederListener.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/NarmestelederListener.kt
@@ -2,11 +2,13 @@ package no.nav.helse.flex.kafka
 
 import no.nav.helse.flex.narmesteleder.OppdateringAvNarmesteLeder
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.context.annotation.Profile
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 
 @Component
+@Profile("test")
 class NarmestelederListener(
     private val oppdateringAvNarmesteLeder: OppdateringAvNarmesteLeder
 ) {
