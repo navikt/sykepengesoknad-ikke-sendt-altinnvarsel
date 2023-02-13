352e1b1be13605339cc9ea8c6e71fc12008064d0
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-02-13 20:33:40 +0100

    Oppgrader til Spring Boot 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,9 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.5"
+    id("org.springframework.boot") version "3.0.2"
     id("io.spring.dependency-management") version "1.1.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.1.0"
     kotlin("jvm") version "1.8.10"
     kotlin("plugin.spring") version "1.8.10"
 }
@@ -13,18 +14,7 @@ version = "1.0.0"
 description = "sykepengesoknad-ikke-sendt-altinnvarsel"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
-buildscript {
-    repositories {
-        maven("https://plugins.gradle.org/m2/")
-    }
-    dependencies {
-        classpath("org.jlleitschuh.gradle:ktlint-gradle:11.1.0")
-    }
-}
-
-ext["okhttp3.version"] = "4.9.0" // For at token support testen kjører
-
-apply(plugin = "org.jlleitschuh.gradle.ktlint")
+ext["okhttp3.version"] = "4.9.3" // Token-support tester trenger Mockwebserver.
 
 val githubUser: String by project
 val githubPassword: String by project
@@ -42,51 +32,62 @@ repositories {
     }
 }
 
-val testContainersVersion = "1.17.6"
-val tokenSupportVersion = "2.1.9"
+val sykepengesoknadKafkaVersion = "2022.12.21-07.53-20bd43a2"
 val logstashLogbackEncoderVersion = "7.2"
+val tjenestespesifikasjonerVersion = "2610.9b6de22"
+val testContainersVersion = "1.17.6"
 val kluentVersion = "1.72"
-val sykepengesoknadKafkaVersion = "2022.10.28-10.24-aa0eced7"
-val tjenestespesifikasjonerVersion = "1.2020.01.20-15.44-063ae9f84815"
+val tokenSupportVersion = "3.0.3"
+val smCommonVersion = "1.1e5e122"
+val gcsVersion = "2.17.2"
+val gcsNioVersion = "0.126.3"
+val commonsTextVersion = "1.10.0"
+val jakartaActivationVersion = "2.0.1"
 val cxfVersion = "3.5.5"
+val bindApiVersion = "2.3.3"
+val jaxbRuntimeVersion = "2.4.0-b180830.0438"
+val jwsApiVersion = "2.1.0"
+val jaxWsApiVersion = "2.3.1"
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.kafka:spring-kafka")
-    implementation("no.nav.security:token-client-spring:$tokenSupportVersion")
-    implementation("no.nav.security:token-validation-spring:$tokenSupportVersion")
-    implementation("org.slf4j:slf4j-api")
-    implementation("org.flywaydb:flyway-core")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.hibernate.validator:hibernate-validator")
     implementation("org.postgresql:postgresql")
-    implementation("org.springframework.boot:spring-boot-starter-logging")
-    implementation("net.logstash.logback:logstash-logback-encoder:$logstashLogbackEncoderVersion")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("no.nav.helse.flex:sykepengesoknad-kafka:$sykepengesoknadKafkaVersion")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.slf4j:slf4j-api")
     implementation("org.aspectj:aspectjrt")
     implementation("org.aspectj:aspectjweaver")
-    implementation("org.hibernate.validator:hibernate-validator")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("io.micrometer:micrometer-registry-prometheus")
-    implementation("org.apache.commons:commons-text:1.10.0")
-
-    implementation("com.sun.activation:jakarta.activation:2.0.1")
-    implementation("com.sun.xml.bind:jaxb-impl:3.0.2")
-    implementation("com.sun.xml.bind:jaxb-core:3.0.2")
-    implementation("com.sun.xml.messaging.saaj:saaj-impl:1.5.1")
-    implementation("javax.xml.ws:jaxws-api:2.3.1")
-    implementation("javax.jws:javax.jws-api:1.1")
-    implementation("no.nav.tjenestespesifikasjoner:behandle-altinnmelding-v1-tjenestespesifikasjon:$tjenestespesifikasjonerVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("no.nav.security:token-client-spring:$tokenSupportVersion")
+    implementation("no.nav.security:token-validation-spring:$tokenSupportVersion")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logstashLogbackEncoderVersion")
+    implementation("no.nav.helse.flex:sykepengesoknad-kafka:$sykepengesoknadKafkaVersion")
+    implementation("org.apache.commons:commons-text:$commonsTextVersion")
+    implementation("com.sun.activation:jakarta.activation:$jakartaActivationVersion")
     implementation("org.apache.cxf:cxf-spring-boot-starter-jaxws:$cxfVersion")
+    implementation("no.nav.tjenestespesifikasjoner:behandle-altinnmelding-v1-tjenestespesifikasjon:$tjenestespesifikasjonerVersion")
 
+    // saaj-impl-2.0.0 uses the jakarta namespace instead of javax.
+    implementation("com.sun.xml.messaging.saaj:saaj-impl:1.5.3")
+    // https://stackoverflow.com/questions/71095913/what-is-the-difference-between-jaxb-impl-and-jaxb-runtime
+    implementation("jakarta.xml.bind:jakarta.xml.bind-api:$bindApiVersion")
+    implementation("org.glassfish.jaxb:jaxb-runtime:$jaxbRuntimeVersion")
+    implementation("jakarta.jws:jakarta.jws-api:$jwsApiVersion")
+    implementation("javax.xml.ws:jaxws-api:$jaxWsApiVersion")
+
+    testImplementation(platform("org.testcontainers:testcontainers-bom:$testContainersVersion"))
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.testcontainers:kafka:$testContainersVersion")
-    testImplementation("org.testcontainers:postgresql:$testContainersVersion")
-    testImplementation("no.nav.security:token-validation-spring-test:$tokenSupportVersion")
+    testImplementation("org.testcontainers:kafka")
+    testImplementation("org.testcontainers:postgresql")
     testImplementation("org.awaitility:awaitility")
+    testImplementation("no.nav.security:token-validation-spring-test:$tokenSupportVersion")
     testImplementation("org.amshove.kluent:kluent:$kluentVersion")
 }
 
