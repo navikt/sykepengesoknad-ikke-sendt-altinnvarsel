98f205e81ca04be8a141758f88b6cbfa979e6795
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2025-05-07 13:17:42 +0200

    Bump apache/kafka-native til 4.0.0

--- src/test/kotlin/no/nav/helse/flex/FellesTestOppsett.kt
+++ src/test/kotlin/no/nav/helse/flex/FellesTestOppsett.kt
@@ -50,7 +50,7 @@ abstract class FellesTestOppsett {
                 System.setProperty("spring.datasource.password", it.password)
             }
 
-            KafkaContainer(DockerImageName.parse("apache/kafka-native:3.8.1")).also {
+            KafkaContainer(DockerImageName.parse("apache/kafka-native:4.0.0")).also {
                 it.start()
                 System.setProperty("KAFKA_BROKERS", it.bootstrapServers)
             }
